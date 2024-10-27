c94187ba44295725eb396afcd9ee9cb56d10f165
Ojasv Dixit <115476649+Ojasvdixit@users.noreply.github.com>
2024-10-27 09:06:06 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,7 +7,7 @@ export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://meditrack-backend-9pg4.onrender.com"
 
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
 
