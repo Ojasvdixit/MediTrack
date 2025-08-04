ab085a4216b4f3bbb49919993274073b9d126232
Ojasv Dixit <115476649+Ojasvdixit@users.noreply.github.com>
2025-08-04 22:16:10 +0530

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 
     mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect("mongodb+srv://Ojasv123:Ojasv123321@cluster0.enjln.mongodb.net/prescripto")
 
 }
 
